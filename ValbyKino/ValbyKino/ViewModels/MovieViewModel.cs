7ed93ba3ed1565719bb36538ebb57b03cfbb97ba
Husein Schuta <Hanihusi2x@gmx.com>
2024-11-27 10:20:20 +0100

    HuseinsView

--- ValbyKino/ValbyKino/ViewModels/MovieViewModel.cs
+++ ValbyKino/ValbyKino/ViewModels/MovieViewModel.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ValbyKino.Models;
 
 namespace ValbyKino.ViewModels
@@ -12,7 +7,7 @@ namespace ValbyKino.ViewModels
     public class MovieViewModel : ViewModelBase
     {
         public ObservableCollection<Movie> Movies { get; set; }
-        public MovieViewModel() 
+        public MovieViewModel()
         {
             Movies = new ObservableCollection<Movie>();
             Movies.Add(new Movie("Crossing", "En Kvinde i Istanbul", "Levan", "Akin", "TR", DateTime.Now, false));
@@ -31,7 +26,59 @@ namespace ValbyKino.ViewModels
                 OnPropertyChanged(nameof(MovieCollectionView));
             }
         }
-        public Movie SelectedMovie { get; set; }
+        //public Movie SelectedMovie { get; set; }
+
+
+        //Jeg laver en privat metode, jeg vil have at metoden tilføjer en ny Movie til Movies samlingen
+        private void AddMovie()
+        {
+            // Movies er samlingen
+            // Add er metoden
+            // new Movie kalder konstruktøren med de nødvendige parametre
+            Movies.Add(new Movie(
+                "Zorro",                        // OriginalTitle
+                "Zorro Den Maskerede Hævner",   // LocalTitle
+                "Martin",                       // DirectorFirstName
+                "Campbell",                     // DirectorLastName
+                "US",                           // OriginalCountry
+                new DateTime(1998, 7, 17),      // NationalReleaseDate
+                true                            // AlternativeContent
+            ));
+        }
+
+        //Det her vil repræsenter, uanset hvilket element er valgt,og lader os ændre dens oplysninger
+        private Movie selectedItem;
+
+
+
+        //Jeg implementer INotifyPropertyChanged og dens interface for at den her kode kan informere GUI og omvendt
+        public Movie? SelectedItem
+        {
+            get { return selectedItem; }
+
+            //Hver gang setter bliver kaldt kalder vi OnPropertyChanged. Hvis selectedItem bliver sat bliver alle passende informeret og vores bindinger vil virke
+            set { selectedItem = value; OnPropertyChanged(); }
+        }
+
+        //Movies er samlingen
+        //Remove er metoden
+        //selectedItem dvs. ting som er valgt
+        private void DeleteMovie()
+        {
+
+            Movies.Remove(selectedItem);
+
+        }
+
+
+        //Vi laver en objekt af vores RelayCommand som vi kalder AddCommand. Jeg sætter AddCommand til at være en new RelayCommand,
+        //execute er sat til at være metoden AddItem som tilføjer nye item til samlingen som hedder items
+        //Fordi vi vil have at AddCommand kan udføres altid, fjerner vi canExecute => (return true;)
+        public RelayCommand AddCommand => new RelayCommand(execute => AddMovie());
+
+        //execute er sat til at være metoden DeleteItem som fjerner  item fra samlingen som hedder items
+        //canExecute her gør at DeleteItem ikke er aktiveret, hvis der ikke er valgt noget, knappen bliver aktiv, når vi har valgt noget
+        public RelayCommand DeleteCommand => new RelayCommand(execute => DeleteMovie(), canExecute => SelectedItem != null);
 
     }
 }
---
+++ ValbyKino/ValbyKino/Views/MovieView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using ValbyKino.ViewModels;
+
+namespace ValbyKino.Views
+{
+    /// <summary>
+    /// Interaction logic for MovieView.xaml
+    /// </summary>
+    public partial class MovieView : Page
+    {
+        public MovieView()
+        {
+            InitializeComponent();
+            DataContext = new MovieViewModel();
+        }
+
+
+    }
+}
