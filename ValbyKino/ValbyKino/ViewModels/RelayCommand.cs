a1e35fe87c493d25d9a5356d5064ab0aae725827
Line Kargaard <lika62310@edu.ucl.dk>
2024-11-27 10:44:54 +0100

    Tilføjet views.

---
+++ ValbyKino/ValbyKino/ViewModels/RelayCommand.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ValbyKino.ViewModels
+{
+    //Jeg bruger buttondCommand og binder her til RelayCommand i ViewModel, hvilket giver os mulighed for at udføre al vores udregning/logik
+    //her, vi slipper for at lave kode i code behind.
+
+    //Vi laver her en klasse RelayCommand, som implementerrer ICommand(), så vi kan lave vores kommandoer i vores ViewModel
+    //Vi skal bruge using System.Windows.Input;
+    //Vi kan implementer ICommand for alle command, i stedet for at bruge minimum krav af interface kan vi gøre det mere alsidigt og kraftfuldt 
+    //DEN HER KODE KAN VI BRUGE I ALLE MVVMprojekter uden at ændre det her overhoved
+
+    public class RelayCommand : ICommand
+    {
+        //Det her er en metode som har en enkelt parameter og som ikke returnerer en værdi. execute er en funktionskald
+        private readonly Action<object> execute;
+
+        //returner en værdi som er specificeret af bool parameteren, vil returnere en bool værdi
+        private readonly Func<object, bool>? canExecute;
+
+        //
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool>? canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        //
+        public bool CanExecute(object? parameter)
+        {
+            //vi kan udføre noget hvis vi ikke har en funktion for canExecute eller vi kan køre canExecute med parameter
+            //
+            return canExecute == null || canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
---
+++ ValbyKino/ValbyKino/Views/MovieView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ValbyKino.ViewModels;
+
+namespace ValbyKino.Views
+{
+    /// <summary>
+    /// Interaction logic for MovieView.xaml
+    /// </summary>
+    public partial class MovieView : Page
+    {
+        public MovieView()
+        {
+            InitializeComponent();
+            DataContext = new MovieViewModel();
+        }
+    }
+}
