834da40db06769df594bc10ee557c2dfb81f91bf
Husein Schuta <Hanihusi2x@gmx.com>
2024-12-13 11:43:31 +0100

    Nye rettelse på pris, annuall rapportview og ting i show

---
+++ ValbyKino/ValbyKino/ViewModels/MainViewModelMainWindow.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using ValbyKino.Views;
+
+namespace ValbyKino.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private object _currentView;
+        public object CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged(nameof(CurrentView));
+            }
+        }
+
+        //Commands til MainWindow
+        public ICommand ShowMovieViewCommand { get; }
+        public ICommand ShowShowViewCommand { get; }
+        public ICommand ShowAnnualReportViewCommand { get; }
+
+
+        //kommandoer bliver (commands) oprettet og tilknyttet funktionalitet (forskellige visninger),
+        //og en startværdi for CurrentView (LoginView) bliver indstillet.
+        public MainWindowViewModel()
+        {
+            //når kommandoen udføres skifter den aktuelle visning (CurrentView) til en instans af MovieView
+            ShowMovieViewCommand = new RelayCommand(_ => CurrentView = new MovieView());
+            ShowShowViewCommand = new RelayCommand(_ => CurrentView = new ShowView());
+            ShowAnnualReportViewCommand = new RelayCommand(_ => CurrentView = new AnnualReportView());
+
+
+            CurrentView = new LoginView();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
